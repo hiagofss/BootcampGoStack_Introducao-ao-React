d8f79ab127921a48aee751b2a34ff782fd0239ad
Hiago Souza <hiagofss98@gmail.com>
2019-07-02 20:41:42 -0300

    Estado & Imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 
-import TeckList from './components/TechList';
+import TechList from './components/TechList';
 
 function App() {
-  return <TeckList />;
+  return <TechList />;
 }
 
 export default App;
